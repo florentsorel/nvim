b100eb73c1c7f7f0329e685212b721a552034008
florentsorel <florent.sorel@gmail.com>
2024-03-08 22:24:43 +0100

    feat: disable hint for gopls

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -165,17 +165,19 @@ return { -- LSP Configuration & Plugins
               unusedvariable = true,
             },
             hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true,
+              assignVariableTypes = false,
+              compositeLiteralFields = false,
+              compositeLiteralTypes = false,
+              constantValues = false,
+              functionTypeParameters = false,
+              parameterNames = false,
+              rangeVariableTypes = false,
             },
           },
         },
       },
+
+      kotlin_language_server = {},
 
       templ = {},
       html = {
