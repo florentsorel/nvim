2722308955f204b7e7c122fa1063f7cf6e45b243
Florent Sorel <florent.sorel@gmail.com>
2024-11-15 00:55:14 +0100

    feat: add border to hover floating window

--- init.lua
+++ init.lua
@@ -90,6 +90,14 @@ vim.keymap.set("n", "K", function()
     end
   end
   if not open_floats then
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.floor(vim.o.lines * 0.3)
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+      max_width = width,
+      max_height = height,
+    })
     vim.lsp.buf.hover()
   end
 end)
