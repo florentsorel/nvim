50ed2112b283730335fbbc6ff86e6f9f297ba5f2
Florent Sorel <florent.sorel@gmail.com>
2025-04-30 17:25:27 +0200

    remove fieldalignement for gopls

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -192,7 +192,6 @@ return {
                 rangeVariableTypes = true,
               },
               analyses = {
-                fieldalignment = true,
                 nilness = true,
                 unusedparams = true,
                 unusedwrite = true,
